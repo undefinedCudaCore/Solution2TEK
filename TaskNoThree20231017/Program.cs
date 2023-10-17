c178407ec9585a334e30010e47dfc540945ebbd9
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-17 18:07:34 +0300

    Done third task

---
+++ TaskNoThree20231017/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter squere length:");
+
+        string getValue = Console.ReadLine();
+
+        int n = int.Parse(getValue);
+
+        for (int row = 1; row <= n; row++)
+        {
+            for (int col = 1; col <= n; col++)
+            {
+                if (row == 1 || row == n)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (col == 1 || col == n)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ TaskNoTwo20231017/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        string vardas = "Mantas";
+
+        Console.WriteLine("Labas " + vardas);
+
+        int amzius = 24;
+
+        Console.WriteLine("Įvestas amžius: {0}", amzius);
+
+        string marke = "Volksvagen";
+        string modelis = "Passat CC";
+        int metai = 2008;
+        int rida = 200000;
+
+        Console.WriteLine("Automobilio {0} {1} ({2} m.) rida - {3} km",marke, modelis, metai, rida);
+    }
+}

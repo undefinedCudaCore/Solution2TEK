0468863ee6e0bd7f5a91784ee83ce45b2cd5ee8f
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-17 17:40:52 +0300

    Done task number one

---
+++ TaskNoOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        // Kintamieji
+        string pavarde = "Pavardenis";
+        int amzius = 24;
+        float ugis = 185.5f;
+        double svoris = 75.63;
+        char simbolis = '*';
+    }
+}

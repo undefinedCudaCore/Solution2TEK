9ccaa2588870932b6d98ac4ee2b10b0e9b2c566b
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-19 15:45:56 +0300

    Add task No 1 and 2

---
+++ TaskNo_07/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine("Nr. 1");
+        Console.WriteLine();
+
+
+        Console.WriteLine("Įveskite savo vardą:");
+        string vardas = Console.ReadLine();
+
+        Console.WriteLine("Įveskite savo amžių:");
+        string amzius = Console.ReadLine();
+
+        Console.WriteLine($"Jūsų varda {vardas}, o amžius {amzius}.");
+
+        Console.WriteLine("Nr. 2");
+        Console.WriteLine();
+
+        Console.WriteLine("Įveskite simbolį:");
+        string simbolis = Console.ReadLine();
+
+        int convertedSimbolis = Convert.ToInt64(simbolis);
+
+        int valueForWhileLoop = convertedSimbolis.MinValue;
+        while (true)
+        {
+            
+        }
+    }
+}

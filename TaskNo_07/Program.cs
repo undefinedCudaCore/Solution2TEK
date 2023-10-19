11fbdd9a4410675fddd01ae0133a629d7410bb1a
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-19 17:29:37 +0300

    Add task 3, 4 and 5

--- TaskNo_07/Program.cs
+++ TaskNo_07/Program.cs
@@ -14,20 +14,60 @@
         Console.WriteLine("Įveskite savo amžių:");
         string amzius = Console.ReadLine();
 
-        Console.WriteLine($"Jūsų varda {vardas}, o amžius {amzius}.");
+        Console.WriteLine($"Jūsų vardas {vardas}, o amžius {amzius}.");
 
         Console.WriteLine("Nr. 2");
         Console.WriteLine();
 
         Console.WriteLine("Įveskite simbolį:");
-        string simbolis = Console.ReadLine();
+        var simbolis = Console.ReadLine();
 
-        int convertedSimbolis = Convert.ToInt64(simbolis);
+        char convertedSimbolis = Convert.ToChar(simbolis);
 
-        int valueForWhileLoop = convertedSimbolis.MinValue;
-        while (true)
+        Console.WriteLine($"Jūsų įvestas simbolis yra '{convertedSimbolis}' .");
+
+        for (int i = 0; i < 3; i++)
         {
-            
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($"{convertedSimbolis}");
+            }
+            Console.WriteLine();
         }
+
+        Console.WriteLine();
+        Console.WriteLine("Nr. 3");
+        Console.WriteLine();
+
+        Console.WriteLine("Įveskite skaičių:");
+
+        string skaiciusKvadratui = Console.ReadLine();
+
+        int konvertuotasSkaiciusKvadratui = Convert.ToInt32(skaiciusKvadratui);
+
+        double skaiciusKvadratu = Math.Pow(konvertuotasSkaiciusKvadratui, 2);
+
+        Console.WriteLine($"Jūsų skaičius kvadratu: {skaiciusKvadratu}.");
+
+        Console.WriteLine();
+        Console.WriteLine("Nr. 4");
+        Console.WriteLine();
+        Console.WriteLine("Įveskite tris skaičius -> įveskite pirmą skaičių ir spauskite'Enter':");
+
+        string pirmasisSkaicius = Console.ReadLine();
+
+        Console.WriteLine("Įveskite antrą skaičių ir spauskite'Enter':");
+
+        string antrasisSkaicius = Console.ReadLine();
+
+        Console.WriteLine("Įveskite trečią skaičių ir spauskite'Enter':");
+
+        string treciasisSkaicius = Console.ReadLine();
+
+        int sum = Convert.ToInt32(pirmasisSkaicius)
+            + Convert.ToInt32(antrasisSkaicius)
+            + Convert.ToInt32(treciasisSkaicius);
+
+        Console.WriteLine($"{pirmasisSkaicius} + {antrasisSkaicius} + {treciasisSkaicius} = {sum}");
     }
 }

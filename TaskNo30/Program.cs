19028d22e7c1f41e4448a26745fbdc54373997d4
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-22 16:19:39 +0300

    Add Task No. 30

---
+++ TaskNo30/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine();
+        Console.WriteLine("Part No. 1");
+        Console.WriteLine();
+
+        Console.WriteLine("Enter first number:");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        Suma(firstNumber, secondNumber);
+        Skirtumas(firstNumber, secondNumber);
+        Sandauga(firstNumber, secondNumber);
+        Dalmuo(firstNumber, secondNumber);
+    }
+
+    private static void Suma(int a, int b)
+    {
+        Console.WriteLine($"{a} + {b} = {a + b}");
+    }
+    private static void Skirtumas(int a, int b)
+    {
+        Console.WriteLine($"{a} - {b} = {a - b}");
+
+    }
+    private static void Sandauga(int a, int b)
+    {
+        Console.WriteLine($"{a} * {b} = {a * b}");
+
+    }
+    private static void Dalmuo(double a, double b)
+    {
+        Console.WriteLine($"{a} / {b} = {Math.Round(a / b, 2)}");
+
+    }
+}

f61fdb4fde94b90dac1dae36f4cabba4a492885d
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-22 16:03:54 +0300

    Add task No. 27

---
+++ TaskNo27/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine();
+        Console.WriteLine("Part No. 1");
+        Console.WriteLine();
+
+        List<string> zodis = new List<string>();
+        zodis.Add("suo");
+        zodis.Add("kate");
+        zodis.Add("multivanas");
+
+        foreach (string zodisSarase in zodis)
+        {
+            Console.WriteLine($"Sudarytas iš {zodisSarase.Length} raidzių.");
+            
+        }
+    }
+}

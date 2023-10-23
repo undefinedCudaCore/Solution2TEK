475fa917bfc092dbc6e7faa72673d5c5ed5114fb
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-23 09:11:03 +0300

    Add task No. 32

---
+++ TaskNo32/Program.cs
@@ -0,0 +1,20 @@
+﻿using TaskNo32;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine();
+        Console.WriteLine("Part No. 1");
+        Console.WriteLine();
+
+        Console.Write("Enter User name: ");
+        string userName = Console.ReadLine();
+
+        User userPhrase = new User();
+
+        Console.WriteLine(userPhrase.UserName(userName));
+    }
+}

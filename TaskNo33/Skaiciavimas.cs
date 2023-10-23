f7401201bd6e2a236389560b1cd97ebb78534434
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-23 09:38:21 +0300

    Add task No. 33

---
+++ TaskNo33/Skaiciavimas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskNo33
+{
+    internal class Skaiciavimas
+    {
+        public int Sudetis(int a, int b)
+        {
+            return a + b;
+        }
+        public int Atimtis(int a, int b)
+        {
+            return a - b;
+        }
+        public double KetvirtuLaipsniu(int a, int b)
+        {
+            double pow = Math.Pow(a + b, 4);
+            return pow;
+        }
+        public double Saknis(int a, int b)
+        {
+            double sqrt = Math.Sqrt(a + b);
+            return sqrt;
+        }
+    }
+}

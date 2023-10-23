b54f235fab260d1fffc7f0a1d7492e18e7ae8ea2
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-23 09:52:00 +0300

    Add task No. 34

---
+++ TaskNo34/Calculation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskNo34
+{
+    internal class Calculation
+    {
+        public int Daugyba(int x, int y)
+        {
+            return x * y;
+        }
+        public double Dalyba(double x, double y)
+        {
+            return Math.Round((x / y), 2);
+        }
+    }
+}
---
+++ TaskNo34/Program.cs
@@ -0,0 +1,45 @@
+﻿using TaskNo34;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine();
+        Console.WriteLine("Part No. 1");
+        Console.WriteLine();
+
+        Console.WriteLine("ENTER first number: ");
+        int firstNumberForTaskThreeFour = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("ENTER second number: ");
+        int secondNumberForTaskThreeFour = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Select an action from list:");
+        Console.WriteLine("1 - Multiplication of two numbers.");
+        Console.WriteLine("2 - Division of two numbers.");
+
+        try
+        {
+            int selection = Convert.ToInt32(Console.ReadLine());
+            Calculation calculation = new Calculation();
+            if(selection == 1)
+            {
+                Console.WriteLine($"Answer is {calculation.Daugyba(firstNumberForTaskThreeFour, secondNumberForTaskThreeFour)}.");
+            } 
+            else if (selection == 2)
+            {
+                Console.WriteLine($"Answer is {calculation.Dalyba(firstNumberForTaskThreeFour, secondNumberForTaskThreeFour)}.");
+            }
+            else
+            {
+                Console.WriteLine("No such action.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("No such action.");
+        }
+    }
+}

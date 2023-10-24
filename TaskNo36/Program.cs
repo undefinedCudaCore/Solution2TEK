410a949f6216f90f9a1e448479a8bd03033446e3
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-24 17:05:57 +0300

    Add count for best and worst grade

--- TaskNo36/Program.cs
+++ TaskNo36/Program.cs
@@ -8,7 +8,7 @@ internal class Program
 
         // Create first student and his information
 
-        List<int> pazymiai1 = new List<int>() { 5, 2, 3, 8, 10 };
+        List<int> pazymiai1 = new List<int>() { 5, 2, 3, 8, 7 };
         Mokinys mokinys = new Mokinys("a", "b", 2, 2, pazymiai1);
 
         mokinys.isvestiInfoApieMokini();
@@ -16,22 +16,22 @@ internal class Program
         // Create second student and his information
 
         List<int> pazymiai2 = new List<int>() { 5, 7, 10, 8, 10 };
-        Mokinys mokinysTomas = new Mokinys("Tomas", "Klaukys", 15, 9, pazymiai1);
+        Mokinys mokinysTomas = new Mokinys("Tomas", "Klaukys", 15, 9, pazymiai2);
 
         mokinysTomas.isvestiInfoApieMokini();
 
         // Create third student and his information
 
-        List<int> pazymiai3 = new List<int>() { 10, 10, 10, 9, 10 };
-        Mokinys mokinysEmilia = new Mokinys("Emilia", "Clarke", 16, 10, pazymiai1);
+        List<int> pazymiai3 = new List<int>() { 10, 10, 8, 9, 10 };
+        Mokinys mokinysEmilia = new Mokinys("Emilia", "Clarke", 16, 10, pazymiai3);
 
         mokinysEmilia.isvestiInfoApieMokini();
 
 
         // Create fourth student and his information
 
-        List<int> pazymiai4 = new List<int>() { 10, 10, 10, 9, 10 };
-        Mokinys mokinysTadas = new Mokinys("Tadas", "Blinda", 7, 1, pazymiai1);
+        List<int> pazymiai4 = new List<int>() { 1, 1, 1, 5, 1 };
+        Mokinys mokinysTadas = new Mokinys("Tadas", "Blinda", 7, 1, pazymiai4);
 
         mokinysTadas.isvestiInfoApieMokini();
 
@@ -40,6 +40,15 @@ internal class Program
         mokinysTomas.uzKiekMetuBaigsMokykla();
         mokinysEmilia.uzKiekMetuBaigsMokykla();
         mokinysTadas.uzKiekMetuBaigsMokykla();
+
+        //Show student best and worst grade
+        Console.WriteLine();
+
+        Console.WriteLine($"Geriausias mokinio {mokinys.vardas} pažimys yra {mokinys.pazymiuSarasas.Max()}, o blogiausias yra {mokinys.pazymiuSarasas.Min()}");
+        Console.WriteLine($"Geriausias mokinio {mokinysEmilia.vardas} pažimys yra {mokinysEmilia.pazymiuSarasas.Max()}, o blogiausias yra {mokinysEmilia.pazymiuSarasas.Min()}");
+        Console.WriteLine($"Geriausias mokinio {mokinysTadas.vardas} pažimys yra {mokinysTadas.pazymiuSarasas.Max()}, o blogiausias yra {mokinysTadas.pazymiuSarasas.Min()}");
+        Console.WriteLine($"Geriausias mokinio {mokinysTomas.vardas} pažimys yra {mokinysTomas.pazymiuSarasas.Max()}, o blogiausias yra {mokinysTomas.pazymiuSarasas.Min()}");
+
 
     }
 }

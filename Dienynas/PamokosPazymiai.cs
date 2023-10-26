b641a3f2cc5925cf906090ce3b61bea7b3ca1ad8
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-26 16:43:16 +0300

    Add class PamokosPazymiai functionality

---
+++ Dienynas/PamokosPazymiai.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dienynas
+{
+    internal class PamokosPazymiai
+    {
+        public string Pamoka;
+        List<double> Pazymiai = new List<double>();
+
+        public PamokosPazymiai(string pamoka, List<double> pazymiai)
+        {
+            Pamoka = pamoka;
+            Pazymiai = pazymiai;
+        }
+
+        public double GautiVidurki()
+        {
+            double suma = 0;
+            for (int i = 0; i < Pazymiai.Count; i++)
+            {
+                suma += Pazymiai[i];
+            }
+            return suma / Pazymiai.Count;
+        }
+    }
+}
---
+++ Dienynas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dienynas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test class PamokosPazymiai functionality
+            List<double> matematikosPazymiai = new List<double>();
+            matematikosPazymiai.Add(1);
+            matematikosPazymiai.Add(10);
+            matematikosPazymiai.Add(8);
+            matematikosPazymiai.Add(6);
+            matematikosPazymiai.Add(5);
+            matematikosPazymiai.Add(9);
+
+            PamokosPazymiai pamokosPazymiai = new PamokosPazymiai("Matematika", matematikosPazymiai);
+
+            double vidurkis = pamokosPazymiai.GautiVidurki();
+
+            Console.WriteLine($"Pamokos {pamokosPazymiai.Pamoka} pazymiu vidurkis yra {vidurkis}");
+            Console.ReadKey();
+
+            //Test
+
+        }
+    }
+}
